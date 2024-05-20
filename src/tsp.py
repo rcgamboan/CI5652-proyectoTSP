a460c53b4d624fb72e725dca382b987caf9593a5
abelzavaleta <abelzavaleta08@gmail.com>
2024-05-19 22:07:03 -0400

    ch130 timelapse

--- src/tsp.py
+++ src/tsp.py
@@ -50,7 +50,7 @@ def run_algorithm(name, current_city, save_graph, show_on_screen, algorithm_func
 if __name__ == "__main__":
 
     # cities = [ "berlin52", "ch130", "tsp225", "pcb442", "pr1002"]
-    cities = ["berlin52"]
+    cities = ["ch130"]
     # cities = []
     
     for currentCity in cities:
