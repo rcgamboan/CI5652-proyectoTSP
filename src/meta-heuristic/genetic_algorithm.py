a6cb925814c4dc988b31e48109659fd56d9eb356
rcgamboan <rcgamboan@gmail.com>
2024-06-04 20:05:05 -0400

    updating comments

--- src/meta-heuristic/genetic_algorithm.py
+++ src/meta-heuristic/genetic_algorithm.py
@@ -1,7 +1,6 @@
 import random
 import numpy as np
 from iteration_utilities import random_permutation
-import random
 
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..')) 
@@ -16,7 +15,7 @@ def initial_population(cities_list, population_size):
     """
     Método para obtener la población inicial a ser usada en el algoritmo genético.
     
-    Genera populationSize individuos aleatoriamente.
+    Genera population_size individuos aleatoriamente.
     
     Parámetros:
     ----------
@@ -141,7 +140,7 @@ def simple_crossover(numCities, parents):
     O(n), donde n es la cantidad de ciudades en el problema.
     """
     # Calcula el punto de corte aleatorio
-    cut_index = round(random.uniform(1, len(numCities) - 1))
+    c = round(random.uniform(1, len(numCities) - 1))
     # cut and fill crossover
     # Lista que almacena los descendientes, se trabajará con dos descendientes inicialmente
     # La primera lista almacena el primer descendiente y la segunda lista almacena el segundo descendiente
@@ -151,25 +150,45 @@ def simple_crossover(numCities, parents):
     # Rellena los descendientes con los elementos de los padres desde el primero hasta el corte
     # En el caso del primero descendiente, se comienza llenando con los elementos del primer padre
     # y se termina con los elementos del segundo padre
-    offsprings1 = parents[0][0:cut_index]
+    offsprings1 = parents[0][0:c]
     for city in parents[1]:
         if city not in offsprings1:
             offsprings1.append(city)
     
     # Rellena el segundo descendiente con los elementos del segundo padre hasta el corte
     # y lo termina con los elementos del primer padre
-    offsprings2 = parents[1][0:cut_index]
+    offsprings2 = parents[1][0:c]
     for city in parents[0]:
         if city not in offsprings2:
             offsprings2.append(city)
 
     return [offsprings1, offsprings2]
 
-def partially_mapped_crossover(numCities, parents):
+def partially_matched_crossover(numCities, parents):
+
+    """
+    Operador de cruce para combinar dos padres utilizando el cruce parcialmente mapeado (PMX).
+    El cruce parcialmente mapeado (PMX) es un método de cruce que preserva la información genética
+    de los padres y garantiza que los descendientes sean permutaciones válidas de las ciudades.
+
+    Parámetros:
+    ----------
+    numCities (int): Cantidad total de ciudades/individuos en el problema.
+    parents (lista de lista de ints): Padres que seran cruzados.
+
+    Returns:
+    ------
+    offsprings (lista de listas de enteros): Descendientes generados después del cruce.
+
+    Complejidad de tiempo:
+    O(n), donde n es la cantidad de ciudades en el problema.
+
+    """
 
     # Se calculan dos puntos de corte aleatorios
     c1, c2 = sorted(random.sample(range(len(numCities)), 2))
     
+    # Se inicializan los descendientes con -1
     offspring1 = [-1] * len(numCities)
     offspring2 = [-1] * len(numCities)
 
@@ -199,57 +218,56 @@ def partially_mapped_crossover(numCities, parents):
 
 def swap_mutation(individual):
     
-        """
-        Operador de mutación para intercambiar dos ciudades en un individuo.
-        La mutación intercambia dos ciudades aleatorias en un individuo.
-    
-        Parámetros:
-        ----------
-        individual (lista de ints): Individuo a ser mutado.
-    
-        Returns:
-        ------
-        mutated_individual (lista de ints): Individuo mutado.
-    
-        Complejidad de tiempo:
-        O(1).
-        """
-    
-        # Selecciona dos ciudades aleatorias para intercambiar
-        point1 = random.randint(0, len(individual) - 1)
-        point2 = random.randint(0, len(individual) - 1)
-    
-        # Intercambia las ciudades
-        individual[point1], individual[point2] = (individual[point2], individual[point1])
-    
-        return individual
+    """
+    Operador de mutación para intercambiar dos ciudades en un individuo.
+    La mutación intercambia dos ciudades aleatorias en un individuo.
+
+    Parámetros:
+    ----------
+    individual (lista de ints): Individuo a ser mutado.
+
+    Returns:
+    ------
+    mutated_individual (lista de ints): Individuo mutado.
+
+    Complejidad de tiempo:
+    O(1).
+    """
+
+    # Selecciona dos ciudades aleatorias para intercambiar
+    c1, c2 = sorted(random.sample(range(len(individual)), 2))
+
+    # Intercambia las ciudades
+    individual[c1], individual[c2] = (individual[c2], individual[c1])
+
+    return individual
 
 def inversion_mutation(individual):
         
-        """
-        Operador de mutación para invertir un segmento de un individuo.
-        La mutación invierte un segmento de un individuo, seleccionando dos puntos de corte
-        aleatorios y revirtiendo el orden de las ciudades entre estos puntos.
-        
-        Parámetros:
-        ----------
-        individual (lista de ints): Individuo a ser mutado.
-        
-        Returns:
-        ------
-        mutated_individual (lista de ints): Individuo mutado.
-        
-        Complejidad de tiempo:
-        O(n), donde n es la cantidad de ciudades en el problema.
-        """
-        
-        # Selecciona dos puntos de corte aleatorios
-        c1, c2 = sorted(random.sample(range(len(individual)), 2))
-        
-        # Invierte el segmento entre los puntos de corte
-        individual[c1:c2] = individual[c1:c2][::-1]
-        
-        return individual
+    """
+    Operador de mutación para invertir un segmento de un individuo.
+    La mutación invierte un segmento del individuo, seleccionando dos puntos de corte
+    aleatorios y revirtiendo el orden de las ciudades entre estos puntos.
+    
+    Parámetros:
+    ----------
+    individual (lista de ints): Individuo a ser mutado.
+    
+    Returns:
+    ------
+    mutated_individual (lista de ints): Individuo mutado.
+    
+    Complejidad de tiempo:
+    O(n), donde n es la cantidad de ciudades en el problema.
+    """
+    
+    # Selecciona dos puntos de corte aleatorios
+    c1, c2 = sorted(random.sample(range(len(individual)), 2))
+    
+    # Invierte el segmento entre los puntos de corte
+    individual[c1:c2] = individual[c1:c2][::-1]
+    
+    return individual
 
 def run_ga(cities_coords, distance_matrix, population_size, numGenerations,crossover_rate, mutation_rate, save_every_10_gen=False, crossover_method="pmx", mutation_method="inversion"):
     
@@ -261,8 +279,8 @@ def run_ga(cities_coords, distance_matrix, population_size, numGenerations,cross
 
     Parámetros:
     ----------
-    cities_coords (lista de tuplas): Coordenadas (x, y) de las ciudades.
-    distance_matrix (lista de listas): Matriz de distancias entre ciudades.
+    cities_coords (lista de lista de floats): Coordenadas (x, y) de las ciudades.
+    distance_matrix (lista de listas de floats): Matriz de distancias entre ciudades.
     population_size (int): Tamaño de la población.
     numGenerations (int): Cantidad de generaciones a evolucionar.
     crossover_rate (float): Proporción de individuos seleccionados para el cruce.
@@ -273,8 +291,8 @@ def run_ga(cities_coords, distance_matrix, population_size, numGenerations,cross
 
     Returns:
     ------
-    selected_individuals (lista de listas): Genotipos de los individuos seleccionados
-                                            después de la evolución.
+    selected_individuals (lista de listas de int): Genotipos de los individuos seleccionados
+                                                   después de la evolución.
 
     Complejidad de tiempo:
     O(numGenerations * population_size * numCities), donde numCities es la cantidad de ciudades en el problema.
@@ -284,7 +302,7 @@ def run_ga(cities_coords, distance_matrix, population_size, numGenerations,cross
 
     population = initial_population(cities_names, population_size)
 
-    for generation in range(0, numGenerations+1):
+    for generation in range(0, numGenerations):
         
         if (generation%10 == 0):
             print("Generation ", generation)
@@ -311,7 +329,7 @@ def run_ga(cities_coords, distance_matrix, population_size, numGenerations,cross
         for i in range(0,len(parents_list), 2):
             # Se llama a la funcion de cruce con los dos padres
             if crossover_method == "pmx":
-                offsprings = partially_mapped_crossover(cities_names,parents_list[i:i+2])
+                offsprings = partially_matched_crossover(cities_names,parents_list[i:i+2])
             else:
                 offsprings = simple_crossover(cities_names,parents_list[i:i+2])
 
@@ -355,15 +373,15 @@ def run_ga(cities_coords, distance_matrix, population_size, numGenerations,cross
                 minimum_distance = min(total_dist_all_individuals)
 
                 shortest_path = selected_individuals[index_minimum]
-                plot_path(cities_coords, shortest_path, minimum_distance, title=f"Generation {generation}")
+                shortest_path.append(shortest_path[0])
+                plot_path(cities_coords, shortest_path, minimum_distance, title=f"GA_{crossover_method}_{mutation_method}_gen{generation}", display_graph = False)
             
     return selected_individuals
 
 ##### Parametros del algoritmo genetico
 
 # Tamaño de la poblacion
-#population_size = [100, 200, 300]
-population_size = [100, 150, 200]
+population_size = [100, 500, 1000]
 # Tasa de cruce, es decir, la proporcion de individuos seleccionados de la poblacion
 # en cada generacion para el cruce
 crossover_rate = [0.5, 0.7, 0.9]
@@ -371,6 +389,10 @@ crossover_rate = [0.5, 0.7, 0.9]
 mutation_rate = [0.2, 0.4, 0.6]
 # Numero de generaciones, es decir, el numero de iteraciones del algoritmo genetico
 numGenerations = [200]
+
+crossover_method = "pmx"
+
+mutation_method = "inversion"
 
 # Nombres de algunos de los problemas TSP disponibles
 cities_names = [
@@ -387,14 +409,6 @@ cities_names = [
     "rbz43748",
     "sra104815"]
 
-"""pi = [1,7,3,2,8,5,9,6,4,10]
-tau = [3,5,4,10,8,1,2,9,6,7]
-cities_names = [i+1 for i in range(len(pi))]
-hijos = partially_mapped_crossover(cities_names,[pi, tau])
-
-print(hijos)
-"""
-
 # Ciclo para obtener los datos de las ciudades y ejecutar el algoritmo genetico
 # Se ejecuta el algoritmo genetico para las primeras 5 ciudades de la lista cities_names
 for i in range(len(cities_names)):
@@ -403,17 +417,18 @@ for i in range(len(cities_names)):
 
     # Se crea el archivo txt donde se almacenaran los resultados
     # de la ciudad, si ya existe, se sobreescribe
-    with open(f"./solutions/{cities_names[i]}_pmx_inversion.txt", "w") as text_file:
+    with open(f"./solutions/{cities_names[i]}_{crossover_method}_{mutation_method}.txt", "w") as text_file:
         text_file.write(f"Running GA with {cities_names[i]} data \n\n")
 
     # Se calcula la matriz de distancias
     distance_matrix = np.array(calcular_distancia(cities_coords))
 
     # Guardar imagenes de la mejor solucion cada 10 generaciones
-    # Las imagenes se guardan actualmente en la carpeta gens del directorio actual
+    # Las imagenes se guardan actualmente en la carpeta de imagenes
+    # ubicada en el directorio del proyecto
     save_every_10_gen = False
     
-    # Se ejecuta para cada ciudad el algorithm genetico con los parametros especificados
+    # Se ejecuta para cada ciudad el algoritmo genetico con los parametros especificados
     iter = 1
     for j in range(len(population_size)):
         for k in range(len(crossover_rate)):
@@ -424,14 +439,16 @@ for i in range(len(cities_names)):
                     print(f"Population size: {population_size[j]}")
                     print(f"Crossover rate: {crossover_rate[k]}")
                     print(f"Mutation rate: {mutation_rate[l]}")
-                    # Se ejecuta el algoritmo genetico con los parametros especificados
+                    
                     ga_solution = run_ga(cities_coords, 
                                         distance_matrix, 
                                         population_size[j],
                                         numGenerations[m], 
                                         crossover_rate[k], 
                                         mutation_rate[l], 
-                                        save_every_10_gen)
+                                        save_every_10_gen,
+                                        crossover_method,
+                                        mutation_method)
 
                     # Al finalizar el algoritmo genetico, se tiene una poblacion de soluciones
                     # Se calcula la distancia total de cada solucion y se selecciona la mejor
@@ -443,7 +460,8 @@ for i in range(len(cities_names)):
                     shortest_path = ga_solution[index_minimum]
                     minimum_distance = min(population_dist)
 
-                    with open(f"./solutions/{cities_names[i]}_pmx_inversion.txt", "a") as text_file:
+                    # Se escribe la informacion de la mejor solucion en un archivo txt
+                    with open(f"./solutions/{cities_names[i]}_{crossover_method}_{mutation_method}.txt", "a") as text_file:
                         text_file.write(f"num_generations = {numGenerations[m]}\n")
                         text_file.write(f"population_size = {population_size[j]}\n")
                         text_file.write(f"crossover_rate = {crossover_rate[k]}\n")
@@ -451,14 +469,11 @@ for i in range(len(cities_names)):
                         text_file.write(f"minimum_distance = {minimum_distance}\n")
                         text_file.write(f"avg_distance = {np.mean(population_dist)}\n")
                         text_file.write("---------------------------------------------\n\n")
-                    print(f"saved solution data in ./solutions/{cities_names[i]}_pmx_inversion.txt")
+                    print(f"saved solution data in ./solutions/{cities_names[i]}_{crossover_method}_{mutation_method}.txt")
 
                     # Mostrar el grafico de la mejor obtenida por el algoritmo genetico
                     show_best_route = False
-                    #plot_path(cities_coords,shortest_path, minimum_distance, f"{cities_names[i]}_best_route",show_best_route)
-                    #print(f"saved {cities_names[i]} best route graph in ../../img/genetic-algorithm/{cities_names[i]}_best_route.png")
-                    plot_path(cities_coords,shortest_path, minimum_distance, f"{cities_names[i]}_gen{numGenerations[m]}_ps{population_size[j]}_cr{crossover_rate[k]}_mr{mutation_rate[l]}",show_best_route)
-                    print(f"saved {cities_names[i]} best route graph in ../../img/genetic-algorithm/{cities_names[i]}_gen{numGenerations[m]}_ps{population_size[j]}_cr{crossover_rate[k]}_mr{mutation_rate[l]}.png")
+                    shortest_path.append(shortest_path[0])
+                    #plot_path(cities_coords,shortest_path, minimum_distance, f"{cities_names[i]}_gen{numGenerations[m]}_ps{population_size[j]}_cr{crossover_rate[k]}_mr{mutation_rate[l]}",show_best_route)
+                    #print(f"saved {cities_names[i]} best route graph in ../../img/genetic-algorithm/{cities_names[i]}_gen{numGenerations[m]}_ps{population_size[j]}_cr{crossover_rate[k]}_mr{mutation_rate[l]}.png")
                     iter += 1
-
-                    #print(f"{cities_names[i]} minimum distance using GA : {minimum_distance}\n")
